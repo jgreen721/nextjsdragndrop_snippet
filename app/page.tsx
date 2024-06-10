a60900bb5a69b1db8a559386577a1c7345585d3c
Justin Green <justingreen@Justins-MacBook-Air.local>
2024-06-09 17:16:25 -0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,139 @@
-import Image from "next/image";
+"use client"
+import React, { useState, useRef } from 'react';
+import Image from 'next/image';
+import { CiMapPin } from "react-icons/ci";
 
-export default function Home() {
+type Destination = {
+  id: number,
+  name: string,
+  location: string,
+  img: string,
+};
+
+
+
+type DestinationItemProps = {
+  destination: Destination,
+  onDragStart:any,
+  onDragOver: any,
+  onDrop: any,
+  isDragging: boolean
+  hoveredItem:boolean
+};
+
+const DestinationItem: React.FC<DestinationItemProps> = ({ destination, onDragStart, onDragOver, onDrop, isDragging,hoveredItem }) => {
+  const customDragRef = useRef<HTMLDivElement>(null);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <li
+    className="relative overflow-hidden">
+      <div id="card-content"
+           className={`overflow-hidden relative cursor-grab rounded-3xl ${isDragging ? "bg-gray-400" : "bg-gray-300"} flex items-center justify-start gap-5 p-2.5 my-2 ${hoveredItem && 'border-b-4 border-sky-500'}`}
+           draggable
+           onDragStart={(e) => {
+            onDragStart(e, destination);
+            e.dataTransfer.setData('itemId',JSON.stringify(destination.id));
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+            if(customDragRef.current) {
+              e.dataTransfer.setDragImage(customDragRef.current, 50, 10);
+       }
+    }
+    }
+    onDragOver={(e) => onDragOver(e,destination)}
+    onDrop={(e) => onDrop(e, destination)}
+
+
+      >
+      <div className="cursor-none pointer-events-none">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src={destination.img}
+          width={100}
+          height={100}
+          alt="destination"
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className='relative width-full h'>
+        <h5 className="font-bold">{destination.name}</h5>
+        <div className="flex items-center justify-start gap-2">
+          <CiMapPin className="border-b border-b-black" />
+          <p className="opacity-50">{destination.location}</p>
+        </div>
       </div>
+      <div ref={customDragRef} className={`flex justify-start items-center gap-2 p-2 bg-white rounded w-auto absolute right-0 text-customColor-dark translate-x-[350px]`}>
+      <Image
+          src={destination.img}
+          width={30}
+          height={30}
+          alt="destination"
+        />  
+         <h5 className="font-bold">{destination.name}</h5>
+      </div>
+    </div>
+    </li>
+  );
+};
+
+export default function Home() {
+  const [destinations, setDestinations] = useState<Destination[]>([
+    { id: 1, name: "Scotland Island", location: "Sydney, Australia", img: "/assets/scotlandislandbeach.png" },
+    { id: 2, name: "The Charles Grand Brasserie & Bar", location: "Lorem ipsum, Dolar", img: "/assets/charlesgrand.png" },
+    { id: 3, name: "Bridge Climb", location: "Dolan, Sit amet", img: "/assets/bridgeclimbreg.png" },
+    { id: 4, name: "Scotland Island", location: "Sydney, Australia", img: "/assets/scotland.png" },
+    { id: 5, name: "Clam Bar", location: "Etcetera veni, Vidi vici", img: "/assets/clam.png" },
+    { id: 6, name: "Vivid Festival", location: "Sydney, Australia", img: "/assets/vivid.png" },
+  ]);
+
+
+  const [draggingItem, setDraggingItem] = useState<Destination | null>(null);
+  const [hoveredItem, setHoveredItem] = useState<Destination | null>(null);
+
+  const handleDragStart = (event: React.DragEvent<HTMLElement>, destination: Destination) => {
+    setDraggingItem(destination);
+    // event.dataTransfer.setData('text/plain', JSON.stringify(destination));
+
+    // console.log("drag started!!!",destination.name)
+
+ 
+  };
+
+  const handleDragOver = (event: React.DragEvent<HTMLElement>,destination:Destination) => {
+    // console.log("items been entered!!",event)
+    event.preventDefault();
+    setHoveredItem(destination)
+  };
+
+  const handleDrop = (event: React.DragEvent<HTMLElement>, destination: Destination) => {
+    event.preventDefault();
+    console.log("ID",destination.id)
+    console.log("drag ended!!!",destination.name)
+    console.log(event.dataTransfer.getData('itemId'));
+
+    if (draggingItem) {
+      let temp = destinations.map((dest)=>dest.id == draggingItem.id ? destination : dest.id == destination.id ? draggingItem : dest);
+      setDestinations(temp);
+      setDraggingItem(null);
+      setHoveredItem(null);
+    }
+  };
+
+  return (
+    <main className="flex items-center justify-center flex-column bg-gray-200 min-h-screen border-solid border-4 border-black">
+      <section className="border-white w-1/2">
+        <ul id="destination-items">
+          {destinations.map(destination => (
+            <DestinationItem
+              key={destination.id}
+              destination={destination}
+              hoveredItem={hoveredItem?.id == destination.id}
+              onDragStart={handleDragStart}
+              onDragOver={handleDragOver}
+              onDrop={handleDrop}
+              isDragging={draggingItem?.id === destination.id}
+            />
+          ))}
+        </ul>
+      </section>
+    
     </main>
   );
 }
